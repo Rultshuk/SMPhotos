30ab29d1d17122be0344841c8ab32090492b2c58
olyaatamanjuk <olyaatamanjuk@gmail.com>
2016-11-11 16:21:56 +0200

    2 - Change menu

--- SMPhotos.Web/Controllers/HomeController.cs
+++ SMPhotos.Web/Controllers/HomeController.cs
@@ -6,13 +6,13 @@ using System.Web.Mvc;
 
 namespace SMPhotos.Web.Controllers
 {
-    public class HomeController : Controller
-    {
-        // GET: Home
-        public ActionResult Index()
-		{ 
+	public class HomeController : Controller
+	{
+		// GET: Home
+		public ActionResult Index()
+		{
 			return View();
-        }
+		}
 		public ActionResult Main()
 		{
 			return View();

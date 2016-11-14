d87a7260aa93a4b5e93b94e92a893baffb839b46
olyaatamanjuk <olyaatamanjuk@gmail.com>
2016-11-14 15:51:00 +0200

    2 - Add data source from Database and change configuration

--- SMPhotos.Web/Controllers/HomeController.cs
+++ SMPhotos.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SMPhotos.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,10 @@ namespace SMPhotos.Web.Controllers
 {
 	public class HomeController : Controller
 	{
+		SMPContext db = new DAL.SMPContext();
+	
 		// GET: Home
+		List<User> list = new List<User>();
 		public ActionResult Index()
 		{
 			return View();
@@ -27,7 +31,20 @@ namespace SMPhotos.Web.Controllers
 		}
 		public ActionResult Admin()
 		{
+			TestData();
+			IEnumerable<User> users = db.Users;
+			ViewBag.Users = users;
+			//ViewBag.Users = db.Users.ToList();
+			//return View(db.Users.ToList());
+			//return View(list);
 			return View();
+		}
+		public void TestData()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				list.Add(new User() { Id = 1,LastName="SomeLastName", FirstName = "SomeName", IsActive = false, Email="somemail@gmail.com" });
+			}
 		}
 		public ActionResult Partial()
 		{

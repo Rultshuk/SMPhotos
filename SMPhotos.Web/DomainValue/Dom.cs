2e8ec695a2a9a6ac626d220fadd083128feb2bc6
John Doe <webhelpadmin@gmail.com>
2016-11-18 17:24:17 +0200

    2 - Change Dom.cs for Register

--- SMPhotos.Web/DomainValue/Dom.cs
+++ SMPhotos.Web/DomainValue/Dom.cs
@@ -5,21 +5,25 @@ using System.Web;
 
 namespace SMPhotos.Web
 {
+	public static class Translation
+	{
+		public const string FirstName = "First Name";
+		public const string LastName = "Last Name";
+		public const string Email = "Email";
+		public const string Password = "Password";
+		public const string PasswordConfirmation = "Password Confirmation";
+		public const string Location = "Country";
+		public const string Register = "Register";
+		public const string SignIn = "Sign In";
+		public const string PleaseSignUp = "Please Sign Up ";
+		public const string ItsFreeJoke = "It's free, don`t be scare.";
+	}
+
 	public static class MVCManager
 	{
 		public static class SessionData
 		{
 			public const string UserContext = "UserContext";
-		}
-
-		public static class Translation
-		{
-			public const string FirstName = "First Name";
-			public const string LastName = "Last Name";
-			public const string Email = "Email";
-			public const string Password = "Password";
-			public const string PasswordConfirmation = "Password Confirmation";
-			public const string Location = "Country";
 		}
 
 		public static class Controller
@@ -31,13 +35,6 @@ namespace SMPhotos.Web
 			public static class Home
 			{
 				public const string Name = "Home";
-			}
-			public static class Register
-			{
-				public const string Name = "Register";
-				public const string SignIn = "Sign In";
-				public const string PSU = "Please Sign Up ";
-				public const string IF = "It's free, don`t be scare.";
 			}
 		}
 		public static class View

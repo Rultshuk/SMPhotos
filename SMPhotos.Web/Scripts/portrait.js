9ae4dfb5c2f3db048c4723e83bd883144f2a5a2c
John Doe <webhelpadmin@gmail.com>
2016-11-27 19:49:46 +0200

    2 - add script to add class portrait to images to crop it

---
+++ SMPhotos.Web/Scripts/portrait.js
@@ -0,0 +1,14 @@
+﻿$(window).load(function () {
+    var img = $('.fancybox-thumb img');
+    img.each(function () {
+        var $this = $(this),
+          width = $this.width(),
+          height = $this.height();
+        console.log(width, height);
+        if (width < height) {
+            $this.addClass('portrait');
+        } else {
+            //$this.addClass('landscape');
+        }
+    });
+});

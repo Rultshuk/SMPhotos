52925a8874e4f0884dca35ea08d600fa3a9249a1
John Doe <webhelpadmin@gmail.com>
2016-11-28 17:30:47 +0200

    2 - edit TestData, edit images

--- SMPhotos.Database/TestData/TestData.sql
+++ SMPhotos.Database/TestData/TestData.sql
@@ -1,7 +1,7 @@
 ﻿--User
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('mail@mail.com', 'password', 'Adminka', 'Super', 'Estonia', 1, 1, 1);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('dmurray1@ed.gov', 'EZCPMHWORojt', 'Dorothy', 'Murray', 'Colombia', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('aaustin2@comsenz.com', 'jLB8hUpGFC', 'Ashley', 'Austin', 'Cameroon', 0, 0, 0);
+INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('mail@mail.com', 'password', 'Adminka', 'Super', 'Ukraine', 1, 1, 1);
+INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('uploader@mail.com', 'uploader', 'Dorothy', 'Murray', 'Honduras', 0, 1, 1);
+INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('user@user.com', 'user', 'Ashley', 'Austin', 'Cameroon', 0, 0, 1);
 INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('awalker3@archive.org', 'HLcHDA0', 'Anna', 'Walker', 'China', 0, 0, 0);
 INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('bhart4@npr.org', 'F39BhZTBbKk', 'Brenda', 'Hart', 'Japan', 0, 0, 0);
 INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('pjacobs5@storify.com', 'gnTVJON', 'Phyllis', 'Jacobs', 'France', 0, 0, 0);
@@ -15,73 +15,28 @@ INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Locatio
 INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('kevansd@pcworld.com', 'SuCBZEO', 'Karen', 'Evans', 'Bolivia', 0, 0, 0);
 INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('dolsone@hubpages.com', 'gN634zx', 'Debra', 'Olson', 'China', 0, 0, 0);
 INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('jriveraf@miitbeian.gov.cn', 'a415Oi', 'Juan', 'Rivera', 'Brazil', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('whowardg@earthlink.net', 'WFghOKAawhyw', 'Wanda', 'Howard', 'Sri Lanka', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('jrichardsonh@reference.com', 'S9wFRM3LX', 'Jeffrey', 'Richardson', 'Tanzania', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('mhunti@google.nl', 'H2LTQb8', 'Maria', 'Hunt', 'South Africa', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('kcarpenterj@irs.gov', 'jbZRYp4Vmjv', 'Kenneth', 'Carpenter', 'Ukraine', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('tphillipsk@state.gov', 'Cek2fsy45pVa', 'Theresa', 'Phillips', 'Bangladesh', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('dfreemanl@macromedia.com', 'dXIB5SDRxkGa', 'Deborah', 'Freeman', 'Indonesia', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('alongm@cmu.edu', 'B4j1knmLm6u', 'Angela', 'Long', 'Indonesia', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('dedwardsn@privacy.gov.au', '8goAhtHb', 'Deborah', 'Edwards', 'China', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('rgrayo@google.pl', 'KikiMZdaY', 'Russell', 'Gray', 'Portugal', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('cevansp@unesco.org', 'OjYDUWOxTUK', 'Clarence', 'Evans', 'Argentina', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('pburnsq@woothemes.com', 'g5NIu0g', 'Phyllis', 'Burns', 'China', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('jharrisr@t.co', 'nTXyRyX', 'Julia', 'Harris', 'Estonia', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('mwrights@sciencedaily.com', 'kKDMnB940D', 'Matthew', 'Wright', 'Indonesia', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('jcolet@squidoo.com', 'PfUhVwbcK0v7', 'Joyce', 'Cole', 'Philippines', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('wbowmanu@fastcompany.com', 'lgtdw1LQmdf', 'Wayne', 'Bowman', 'New Zealand', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('jhernandezv@noaa.gov', 'MJCyeQ', 'Judy', 'Hernandez', 'Peru', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('amurphyw@prnewswire.com', 'VDEsIr4nu', 'Alan', 'Murphy', 'Democratic Republic of the Congo', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('lpowellx@myspace.com', 'UTVKYAu2sGO', 'Lois', 'Powell', 'Bulgaria', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('tbryanty@ning.com', 'sTm99JfRFux', 'Thomas', 'Bryant', 'Sweden', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('efloresz@xinhuanet.com', '53ySRftm', 'Eric', 'Flores', 'Indonesia', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('cmoore10@cpanel.net', 'v8fSJS', 'Carl', 'Moore', 'Tunisia', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('bjames11@trellian.com', 'ieTxwE6S', 'Betty', 'James', 'Argentina', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('jberry12@google.co.jp', '6uwJ1dguL', 'Joseph', 'Berry', 'Spain', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('jjenkins13@51.la', 'IxzyGVHm', 'Janice', 'Jenkins', 'Indonesia', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('lmendoza14@bloglines.com', 's132y5LuP', 'Linda', 'Mendoza', 'Indonesia', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('mmedina15@tumblr.com', 'XD7QfX', 'Martha', 'Medina', 'Moldova', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('eray16@washington.edu', 'KGNz7PzY7J', 'Emily', 'Ray', 'Poland', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('rfowler17@cbslocal.com', 'LWKAj54c', 'Robin', 'Fowler', 'Indonesia', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('jburns18@vistaprint.com', 'mZPINOQj6c8', 'Juan', 'Burns', 'Peru', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('scook19@ifeng.com', '6GPziU9HHn', 'Steven', 'Cook', 'Portugal', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('mpeterson1a@goo.gl', '5qDnjLPa', 'Marie', 'Peterson', 'China', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('nwebb1c@google.co.jp', 'zFSEEX2bz', 'Nicole', 'Webb', 'Philippines', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('promero1b@meetup.com', 'pHG3Wxa7ab', 'Peter', 'Romero', 'Russia', 0, 0, 0);
-INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('cberry1d@zimbio.com', 'nTZoP0ziSh2N', 'Cheryl', 'Berry', 'United States', 0, 0, 0);
+INSERT INTO [smp].[User] ([Email], [Password], [FirstName], [LastName], [Location], [IsAdmin], [IsActive], [IsUploader]) VALUES ('wbowmanu@fastcompany.com', 'lgtdw1LQmdf', 'Wayne', 'Bowman', 'New Zealand', 0, 0, 1);
 
 --Album
 SET IDENTITY_INSERT [smp].[Album] ON
-	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (1, 'First', 'First Album', 'App_Data', 'DE78FD3C-4534-4A68-ADC4-EFCC72F89363');
-	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (2, 'Second', 'Second Album', 'App_Data', '39D09D90-B53C-47D8-BAB6-F96630F9BC9B');
-	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (3, 'Third', 'Third Album', 'App_Data', 'FB3840F6-67B5-4F49-A0DD-707895F680CB');
-	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (4, 'Fourth', 'Fourth Album', 'App_Data', 'E5AF00CC-726E-4252-A13E-459C6567454A');
-	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (5, 'Fifth', 'Fifth Album', 'App_Data', 'F56BD610-5320-48DD-B4CD-2825438A70BE');
-	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (6, 'Sixth', 'Sixth Album', 'App_Data', '2D62A4DB-EC30-41D6-A9D9-C1E037B44A17');
-	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (7, 'Seventh', 'Seventh Album', 'App_Data', '70B9EF63-F689-4CFB-9D6F-5ED125D491B2');
-	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (8, 'Eighth', 'Eighth Album', 'App_Data', '421ACA62-F921-4C0A-B212-D0B5081E0A25');
-	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (9, 'Ninth', 'Ninth Album', 'App_Data', 'FF2FE1F2-7B29-4910-83DB-FD7E0A86D116');
-	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (10,'Tenth', 'Tenth Album', 'App_Data', '4485B513-7817-4483-B7C9-AF99BD0F15EA');
-	
+	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (1, 'Nature', 'Nature', 'App_Data', 'DE78FD3C-4534-4A68-ADC4-EFCC72F89363');
+	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (2, 'Students', 'C# IT Academy 2016', 'App_Data', '39D09D90-B53C-47D8-BAB6-F96630F9BC9B');
+	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (3, 'Animals', 'Animals', 'App_Data', 'FB3840F6-67B5-4F49-A0DD-707895F680CB');
+	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (4, 'Amsterdam', 'Amsterdam', 'App_Data', 'E5AF00CC-726E-4252-A13E-459C6567454A');
+	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (5, 'Hannover', 'Hannover', 'App_Data', 'F56BD610-5320-48DD-B4CD-2825438A70BE');
+	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (6, 'Old man', 'Old man', 'App_Data', '2D62A4DB-EC30-41D6-A9D9-C1E037B44A17');
+	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (7, 'Dog', 'Dog', 'App_Data', '70B9EF63-F689-4CFB-9D6F-5ED125D491B2');
+	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (8, 'Cat', 'Cat', 'App_Data', '421ACA62-F921-4C0A-B212-D0B5081E0A25');
+	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (9, 'Horse', 'Horse', 'App_Data', 'FF2FE1F2-7B29-4910-83DB-FD7E0A86D116');
+	INSERT INTO [smp].[Album] ([Id], [Name], [Description], [Path], [Guid]) VALUES (10,'Artist', 'Artist', 'App_Data', '4485B513-7817-4483-B7C9-AF99BD0F15EA');
 SET IDENTITY_INSERT [smp].[Album] OFF
 
 --Image
 INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('1.jpg', 'C# is the best', 1);
-INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('2.jpg', 'new photos', 1);
 INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('3.jpg', 'my desc of photos', 1);
 INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('4.jpg', 'First Album', 1);
 INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('5.jpg', 'First Album', 1);
-INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('6.jpg', 'First Album', 1);
-INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('7.jpg', 'First Album', 1);
-INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('8.jpg', 'First Album', 1);
-INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('9.jpg', 'First Album', 1);
-INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('10.jpg', 'First Album', 1);
-INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('3200_2600.png', '3200_2600', 1);
-INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('800_533.png', '800*533', 1);
-INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('1600_1200.png', '1600*1200', 1);
-INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('1600_3200.png', '1600*3200', 1);
-INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('1200_1600.png', '1200*1600', 1);
-INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('1.jpg', 'Second Album', 2);
+INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('2.jpg', 'First Album', 3);
 INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('1.jpg', 'Third Album', 3);
 INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('1.jpg', 'Fourth Album', 4);
 INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('1.jpg', 'Fifth Album', 5);
@@ -90,3 +45,12 @@ INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('1.jpg', 'S
 INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('1.jpg', 'Eighth Album', 8);
 INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('1.jpg', 'Ninth Album', 9);
 INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('1.jpg', 'Tenth Album', 10);
+INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('20161128_163219_2016-11-2815.30.00.jpg', 'Tenth Album', 2);
+INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('20161128_163221_2016-11-2815.47.59.jpg', 'Tenth Album', 2);
+INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('20161128_163221_2016-11-2815.47.48.jpg', 'Tenth Album', 2);
+INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('20161128_163220_2016-11-2815.47.43.jpg', 'Tenth Album', 2);
+INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('20161128_163220_2016-11-2815.30.12.jpg', 'Tenth Album', 2);
+INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('20161128_163209_2016-11-2815.30.08.jpg', 'Tenth Album', 2);
+INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('2.jpg', 'new photos', 1);
+INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('7.jpg', 'First Album', 2);
+INSERT INTO [smp].[Image] ([Name], [Description], [AlbumId]) VALUES ('1.jpg', 'Second Album', 2);

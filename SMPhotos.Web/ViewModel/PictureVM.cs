94cdc97d6fb869279ae5f0b32d7f2aa1e45359d3
John Doe <webhelpadmin@gmail.com>
2016-11-28 14:07:59 +0200

    2 - add menu to image load

--- SMPhotos.Web/ViewModel/PictureVM.cs
+++ SMPhotos.Web/ViewModel/PictureVM.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web;
+using SMPhotos.DAL;
 
 namespace SMPhotos.Web.ViewModel
 {
-	public class PictureVM
+	public class PictureVM : Base
 	{
 		public Guid Guid { get; set; }
 		public string Message { get; set; }
